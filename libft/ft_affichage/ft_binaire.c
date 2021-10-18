80d313b8cdfa1bb6bb8339e933c1c30e02b22da7
marcobonnet <webbonnet@gmail.com>
2021-10-18 10:10:49 +0200

    projet a la norme

--- libft/ft_affichage/ft_binaire.c
+++ libft/ft_affichage/ft_binaire.c
@@ -6,7 +6,7 @@
 /*   By: mbonnet <mbonnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 08:01:26 by mbonnet           #+#    #+#             */
-/*   Updated: 2021/10/14 12:48:59 by mbonnet          ###   ########.fr       */
+/*   Updated: 2021/10/18 10:09:35 by mbonnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	ft_affichage_binaire(char c)
 	while (x <= 7)
 	{
 		bit = (c >> x++) & 1;
-		ft_printf("(%d)", bit);	
+		ft_printf("(%d)", bit);
 	}
 	ft_printf("\n");
 }
